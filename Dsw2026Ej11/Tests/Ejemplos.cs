bcd933e24a2e4d87d2700cfafdce4e82d3c75a7a
MarianoRuiz <marianoruizcoronel@gmail.com>
2026-07-30 14:25:41 -0300

    Fix: correcion de la llamada de metodos de los casos

--- Dsw2026Ej11/Tests/Ejemplos.cs
+++ Dsw2026Ej11/Tests/Ejemplos.cs
@@ -46,7 +46,7 @@ internal class Ejemplos
         Console.WriteLine("");
 
         try { var aux1 = casoList.buscarAlumnoNombre("Roberto") ?? throw new Exception(); }
-        catch (Exception ex)
+        catch (Exception)
         {
             Console.WriteLine("Nombre no encontrado en la lista // NO EXISTE ");
         }
@@ -88,7 +88,7 @@ internal class Ejemplos
         Console.WriteLine("");
         Console.WriteLine("Se borra el primero");
         Console.WriteLine("");
-        casoList.eliminarAlumnoPorPoslista(1);
+        casoList.eliminarAlumnoPorPoslista(0);
 
         Console.WriteLine("Lista:");
 
@@ -116,33 +116,32 @@ internal class Ejemplos
 
         foreach (var alumno in diccionario.retornarDiccionario())
         {
-            Console.WriteLine($"Alumno: {alumno.ToString()}");
+            Console.WriteLine($"Legajo: {alumno.Key}, Nombre: {alumno.Value.Nombre}, Promedio: {alumno.Value.Promedio}");
         }
         Console.WriteLine("");
         Console.WriteLine("Listo");
         EsperarTecla();
         // ----------------------------------------------------------------
         Console.Clear();
-        Console.WriteLine("Buscar un alumno por clave y mostrar por consola");
+        Console.WriteLine("Buscar un alumno por clave (clave = 2) y mostrar por consola");
         Console.WriteLine($"El alumno es { diccionario.buscarAlumno(2).Nombre }");
         EsperarTecla();
         // ------------------------------------------------------------------
         Console.Clear();
-        Console.WriteLine("Buscar un alumno por clave y mostrar por consola que no existe");
-        try { var aux = diccionario.buscarAlumno(5) ?? throw new Exception(); }
-        catch (Exception ex)
+        Console.WriteLine("Buscar un alumno por clave (clave = 5) y mostrar por consola que no existe");
+        try { var aux = diccionario.buscarAlumno(5); }
+        catch (KeyNotFoundException)
         {
             Console.WriteLine("Alumno no existe");
         }
         // ----------------------------------------------------------------------
         Console.Clear();
-        Console.WriteLine("");
-        Console.WriteLine("Eliminar alumno, maria");
+        Console.WriteLine("Eliminar alumno, maria:");
         diccionario.eliminarAlumno(alu2.Id);
 
         foreach (var alumno in diccionario.retornarDiccionario())
         {
-            Console.WriteLine($"Legajo;{alumno.Key}");
+            Console.WriteLine(alumno.ToString());
         }
 
 
@@ -154,24 +153,25 @@ internal class Ejemplos
     {
         var casoLinq = new CasoLinq();
         var libros = new List<Libro>();
-        Console.WriteLine($"El primer libro es {casoLinq.obtenerPrimero()}");
+        Console.WriteLine($"El primer libro es {casoLinq.GetPrimero()}");
         Console.WriteLine("");
-        Console.WriteLine($"El ultimo libro es {casoLinq.obtenerUltimo()}");
+        Console.WriteLine($"El ultimo libro es {casoLinq.GetUltimo()}");
         Console.WriteLine("");
-        Console.WriteLine($"La suma de los precios es {casoLinq.sumaPrecios()}");
+        Console.WriteLine($"La suma de los precios es {casoLinq.GetTotalPrecios()}");
         Console.WriteLine("");
-        Console.WriteLine($"El promedio de los precios es {casoLinq.promedioPrecios()}");
-        casoLinq.GetListById(2).ForEach(libro => Console.WriteLine($"Libro con ID 2: {libro}"));
+        Console.WriteLine($"El promedio de los precios es {casoLinq.GetPromedioPrecios()}");
         Console.WriteLine("");
-        Console.WriteLine("Mostrar libros:");
+        casoLinq.GetListById().ForEach(libro => Console.WriteLine($"Libro con ID mayor a 15: {libro}"));
         Console.WriteLine("");
-        Console.WriteLine($"Mostrar el mayor precio {casoLinq.GetMayorPrecio()}");
+        Console.WriteLine("Mostrar libros (titulo y precio en formato moneda):");
         Console.WriteLine("");
-        Console.WriteLine($"Mostrar el menor precio {casoLinq.GetMenorPrecio()}");
+        Console.WriteLine($"El libro con el mayor precio es {casoLinq.GetMayorPrecio()}");
         Console.WriteLine("");
-        casoLinq.librosMayorPromedio().ForEach(libro => Console.WriteLine($"Libros con precio mayor al promedio: {libro}"));
+        Console.WriteLine($"El libro con el menor precio es {casoLinq.GetMenorPrecio()}");
         Console.WriteLine("");
-        casoLinq.OrdenDescendente().ForEach(libro => Console.WriteLine($"Libros ordenados de mayor a menor precio: {libro}"));
+        casoLinq.GetMayorPromedio().ForEach(libro => Console.WriteLine($"Libros con precio mayor al promedio: {libro}"));
+        Console.WriteLine("");
+        casoLinq.GetOrdenadosPorTitulo().ForEach(libro => Console.WriteLine($"Libros ordenados por titulo de forma descendente: {libro}"));
 
 
     }
